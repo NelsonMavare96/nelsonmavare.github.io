119aeef15b0b346f6555b4532cca21d7d8844156
Nelson Mavare <nelsonmavare96@gmail.com>
2022-02-01 16:41:58 -0400

    Updates

---
+++ precache-manifest.af539c38ab80b1109e0166e0f6cdb49f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4085675537bae25880e42a52ce4ec0af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9291a0eb8defbc3c403a",
+    "url": "/static/css/2.b97dddfb.chunk.css"
+  },
+  {
+    "revision": "4b90b07ad98d1e468572",
+    "url": "/static/css/main.76da946d.chunk.css"
+  },
+  {
+    "revision": "9291a0eb8defbc3c403a",
+    "url": "/static/js/2.b80c9cf0.chunk.js"
+  },
+  {
+    "revision": "8243b127a6edf462d4c75ec12cd9a5eb",
+    "url": "/static/js/2.b80c9cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b90b07ad98d1e468572",
+    "url": "/static/js/main.9b37676d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "bdef88e717a06464e61ad9f7c8f126b0",
+    "url": "/static/media/background-image.bdef88e7.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/static/media/ionicons.c037dbbc.svg"
+  },
+  {
+    "revision": "3bd03784179298de2ae9de7910baf683",
+    "url": "/static/media/nature-sprite.3bd03784.png"
+  },
+  {
+    "revision": "48448f3d46a3ac8a3d2f1ae8ad079e5a",
+    "url": "/static/media/urban-sprite.48448f3d.png"
+  }
+]);
